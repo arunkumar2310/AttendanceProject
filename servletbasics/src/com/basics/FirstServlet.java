a0d9811d76f60b28d7795dbd62398ee2b2e98b87
arunkumar2310 <arunc231097@gmail.com>
2018-07-13 20:41:17 +0530

    Attendance project using JDBC,Servlet and JSP

---
+++ servletbasics/src/com/basics/FirstServlet.java
@@ -0,0 +1,35 @@
+package com.basics;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FirstServlet extends HttpServlet {
+
+	public FirstServlet() {
+		
+	}
+	
+	@Override
+	public void init() throws ServletException {
+		System.out.println("init");
+		super.init();
+	}
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out=resp.getWriter(); 
+		out.println("<h1>Welcome to servlet"+new Date()+"</h1>");
+		System.out.println("doGet");
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
